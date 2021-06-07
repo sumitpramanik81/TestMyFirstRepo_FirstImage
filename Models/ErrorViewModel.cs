371a8d2fa0ea1da93f4b31231118076d17eae6fd
Sumit P <sumitpramanik19818@outlook.com>
2021-06-07 13:35:16 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestMyFirstRepo_FirstImage.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
